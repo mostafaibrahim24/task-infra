94d1aaea11882c27d4adb8a274a6d8bd2bddde9c
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2025-11-21 19:44:04 +0200

    Add outputs

---
+++ modules/app-infra/outputs.tf
@@ -0,0 +1,15 @@
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.endpoint
+}
+
+output "rds_port" {
+  value = aws_db_instance.mysql.port
+}
